73a7adec2026ea78eb5d1448e397fa89f90865a6
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2024-12-21 14:46:46 +0000

    Fixed navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,28 +1,25 @@
 'use client'
 import React from 'react'
-import { Input } from './ui/input'
+// import { Input } from './ui/input'
 import { Button } from './ui/button'
 import Image from 'next/image'
-import { Moon, Sun, User} from "lucide-react"
+// import { Moon, Sun, User} from "lucide-react"
 import Link from 'next/link'
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger
-} from "@/components/ui/dropdown-menu"
+// import {
+//     DropdownMenu,
+//     DropdownMenuContent,
+//     DropdownMenuItem,
+//     DropdownMenuTrigger
+// } from "@/components/ui/dropdown-menu"
 import { buttonVariants } from './ui/button'
 
-
-type Props = {}
-
-const Navbar = (props: Props) => {
+const Navbar = () => {
     return (
         <div className='max-w-[1280px] mx-auto'>
             <div className='flex items-center py-4 gap-10 justify-between'>
-                <a href='/'>
+                <Link href='/'>
                     <Image src='/photos/cwLighting-light.png' width={100} height={40} alt='Logo Image' />
-                </a>
+                </Link>
                 <div className='space-x-3'>
                     <Button asChild>
                         <Link href='/'>Home</Link>
