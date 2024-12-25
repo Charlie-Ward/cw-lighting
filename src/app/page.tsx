ec7364186f40efa0df369826e83bd6b4e9284c63
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2024-12-25 14:24:49 +0000

    Updated image structure and added about hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{
-                duration: 0.4,
+                duration: 1.2,
                 scale: { type: "spring", visualDuration: 0.4, bounce: 0.6 },
           }}>
           <Carousel
@@ -52,43 +52,66 @@ export default function Home() {
             <CarouselContent>
               <CarouselItem>
                 <AspectRatio ratio={16/9}>
-                  <img src="/photos/carousel/slide-1.jpeg" alt="A Gobo on a curtain"/>
+                  <img src="/photos/home/carousel/slide-1.jpeg" alt="A Gobo on a curtain"/>
                 </AspectRatio>
               </CarouselItem>
               <CarouselItem>
                 <AspectRatio ratio={16/9}>
-                  <img src="/photos/carousel/slide-2.jpeg" alt="A Gobo on a curtain"/>
+                  <img src="/photos/home/carousel/slide-2.jpeg" alt="A Gobo on a curtain"/>
                 </AspectRatio>
               </CarouselItem>
               <CarouselItem>
                 <AspectRatio ratio={16/9}>
-                  <img src="/photos/carousel/slide-3.JPG" alt="A Gobo on a curtain"/>
+                  <img src="/photos/home/carousel/slide-3.JPG" alt="A Gobo on a curtain"/>
                 </AspectRatio>
               </CarouselItem>
               <CarouselItem>
                 <AspectRatio ratio={16/9}>
-                  <img src="/photos/carousel/slide-4.jpeg" alt="A Gobo on a curtain"/>
+                  <img src="/photos/home/carousel/slide-4.jpeg" alt="A Gobo on a curtain"/>
                 </AspectRatio>
               </CarouselItem>
               <CarouselItem>
                 <AspectRatio ratio={16/9}>
-                  <img src="/photos/carousel/slide-5.jpeg" alt="A Gobo on a curtain"/>
+                  <img src="/photos/home/carousel/slide-5.jpeg" alt="A Gobo on a curtain"/>
                 </AspectRatio>
               </CarouselItem>
               <CarouselItem>
                 <AspectRatio ratio={16/9}>
-                  <img src="/photos/carousel/slide-6.jpeg" alt="A Gobo on a curtain"/>
+                  <img src="/photos/home/carousel/slide-6.jpeg" alt="A Gobo on a curtain"/>
                 </AspectRatio>
               </CarouselItem>
               <CarouselItem>
                 <AspectRatio ratio={16/9}>
-                  <img src="/photos/carousel/slide-7.jpeg" alt="A Gobo on a curtain"/>
+                  <img src="/photos/home/carousel/slide-7.jpeg" alt="A Gobo on a curtain"/>
                 </AspectRatio>
               </CarouselItem>
             </CarouselContent>
             <CarouselPrevious/>
             <CarouselNext />
           </Carousel>
+        </motion.div>
+      </div>
+      <div className="max-w-[864px] mx-auto pb-32">
+        <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+                duration: 1.2,
+                scale: { type: "spring", visualDuration: 0.4, bounce: 0.6 },
+          }}>
+        <section className="">
+          <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+              <div className="mr-auto place-self-center lg:col-span-7">
+                  <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white">About</h1>
+                  <p className="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl text-gray-400">Charlie Ward is a theatre technician based in the Leicestershire area. With experience in both theatre and other types of stage entertainment he is the go to place for all your technical needs. With having training on both EOS and MagicQ lighting boards, Charlie is well equipped to handle most lighting solutions.</p>
+              </div>
+              <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+                <AspectRatio ratio={16/9}>
+                  <img src="/photos/home/mamma_mia.jpeg" alt="mockup" className="rounded-3xl" />
+                </AspectRatio>
+              </div>                
+          </div>
+      </section>
         </motion.div>
       </div>
     </div>
