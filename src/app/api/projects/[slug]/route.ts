9d42d3e067a97b5d4ad844eaa0efa17da7f0a5ae
Charlie Ward <charlieward0101@gmail.com>
2025-02-14 21:41:45 +0000

    Vercel v3

--- src/app/api/projects/[slug]/route.ts
+++ src/app/api/projects/[slug]/route.ts
@@ -1,11 +1,12 @@
-//route.ts
-//Copyright (C) 2025  Charlie Ward GPL v3
-//Full License @ https://github.com/Charlie-Ward/cw-lighting/blob/main/LICENSE
+// route.ts
+// Copyright (C) 2025  Charlie Ward GPL v3
+// Full License @ https://github.com/Charlie-Ward/cw-lighting/blob/main/LICENSE
 
 import { NextResponse } from 'next/server';
 import { getProjectData } from '../../../../lib/mdxUtils';
+import { NextRequest } from 'next/server';
 
-export async function GET(request: Request, { params }: { params: { slug: string } }) {
+export async function GET(request: NextRequest, { params }: { params: { slug: string } }) {
   // Directly access params.slug
   const slug = params.slug;
 
