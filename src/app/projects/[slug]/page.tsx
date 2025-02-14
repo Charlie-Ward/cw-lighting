7736d002a3771a4af56c6a231259ba0112f2edb1
Charlie Ward <charlieward0101@gmail.com>
2025-02-14 21:51:36 +0000

    vercel v5

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,33 +1,25 @@
-//page.tsx
-//Copyright (C) 2025  Charlie Ward GPL v3
-//Full License @ https://github.com/Charlie-Ward/cw-lighting/blob/main/LICENSE
-
+// app/projects/[slug]/page.tsx
 'use client';
 
 import { useEffect, useState } from 'react';
 import { notFound } from 'next/navigation';
-import Modal from '@/components/modal'; // Import the Modal component
-import { useParams } from 'next/navigation'; // Import useParams
 
 type ProjectProps = {
   title: string;
   content: string;
   images: string[];
   date: string;
+  webDate: string;
   location: string;
   roles: string;
-  webDate: string;
 };
 
-const ProjectPage = () => {
-  const { slug } = useParams(); // Unwrap params using useParams
+const ProjectPage = ({ params }: { params: { slug: string } }) => {
   const [projectData, setProjectData] = useState<ProjectProps | null>(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchProjectData = async () => {
-      const response = await fetch(`/api/projects/${slug}`);
+      const response = await fetch(`/api/projects/${params.slug}`);
       if (response.ok) {
         const data = await response.json();
         setProjectData(data);
@@ -37,21 +29,11 @@ const ProjectPage = () => {
     };
 
     fetchProjectData();
-  }, [slug]);
+  }, [params.slug]);
 
-  const openModal = (image: string) => {
-    setSelectedImage(image);
-    setIsModalOpen(true);
-  };
+  if (!projectData) return <p>Loading...</p>;
 
-  const closeModal = () => {
-    setIsModalOpen(false);
-    setSelectedImage(null);
-  };
-
-  if (!projectData) return <p className="text-white">Loading...</p>;
-
-  const { title, content, images, location, roles, webDate } = projectData;
+  const { title, content, images, webDate, location, roles } = projectData;
 
   return (
     <div className="min-h-screen flex flex-col items-center p-8">
@@ -60,20 +42,9 @@ const ProjectPage = () => {
       <div className="prose prose-invert" dangerouslySetInnerHTML={{ __html: content }} />
       <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {images.map((img, index) => (
-          <img 
-            key={index} 
-            src={img} 
-            alt={`${title} image ${index + 1}`} 
-            className="w-full h-48 object-cover rounded-lg cursor-pointer"
-            onClick={() => openModal(img)} 
-          />
+          <img key={index} src={img} alt={`${title} image ${index + 1}`} className="w-full h-48 object-cover rounded-lg" />
         ))}
       </div>
-      <Modal 
-        isOpen={isModalOpen} 
-        onClose={closeModal} 
-        imageUrl={selectedImage || ''} 
-      />
     </div>
   );
 };
