a1b444b8b593dc2771116528226e31663c4a852a
Charlie Ward <charlieward0101@gmail.com>
2025-02-14 21:28:09 +0000

    made home page dynamic now works with mobile devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
-'use client'
+//page.tsx
+//Copyright (C) 2025  Charlie Ward GPL v3
+//Full License @ https://github.com/Charlie-Ward/cw-lighting/blob/main/LICENSE
+
+'use client';
 import Image from "next/image";
 import {
   Carousel,
@@ -6,112 +10,104 @@ import {
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "@/components/ui/carousel"
+} from "@/components/ui/carousel";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
-import {motion} from "framer-motion";
-import AutoPlay from "embla-carousel-autoplay"
-
+import { motion } from "framer-motion";
+import AutoPlay from "embla-carousel-autoplay";
 
 export default function Home() {
   return (
     <div>
       <section className="">
-        <div className=" mx-auto max-w-screen-xl text-center  ">
-            <motion.div
+        <div className="mx-auto max-w-screen-xl text-center">
+          <motion.div
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{
-                duration: 0.4,
-                scale: { type: "spring", visualDuration: 0.4, bounce: 0.6 },
-            }}>
-              <h1 className="mb-4 text-6xl font-extrabold tracking-tight leading-none text-white md:text-7xl lg:text-8xl dark:text-white">Illuminate</h1>
-              <div className="grid grid-cols-4 grid-rows-2 gap-0">
-                <p className=" text-2xl font-normal text-gray-400 lg:text-3xl sm:px-16 xl:px-48 dark:text-gray-400 col-span-2 pt-16">Welcome to CW Lighting, where creativity meets technical expertise.</p>
-                <Image src="/photos/lthDesk.jpeg" width={540} height={960} alt="Charlie Ward at a lighting desk" className="col-span-2 row-span-2 col-start-3 rounded-full"/>
-              </div>
-            </motion.div>
+              duration: 0.4,
+              scale: { type: "spring", visualDuration: 0.4, bounce: 0.6 },
+            }}
+          >
+            <h1 className="mb-4 text-6xl font-extrabold tracking-tight leading-none text-white md:text-7xl lg:text-8xl dark:text-white">Illuminate</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-0">
+              <p className="text-2xl font-normal text-gray-400 lg:text-3xl sm:px-16 xl:px-48 dark:text-gray-400 pt-16">
+                Welcome to CW Lighting, where creativity meets technical expertise.
+              </p>
+              <Image
+                src="/photos/lthDesk.jpeg"
+                width={540}
+                height={960}
+                alt="Charlie Ward at a lighting desk"
+                className="rounded-full"
+              />
+            </div>
+          </motion.div>
         </div>
-    </section>
+      </section>
       <div className="max-w-[864px] mx-auto pb-32">
         <motion.div
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-                duration: 1.2,
-                scale: { type: "spring", visualDuration: 0.4, bounce: 0.6 },
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 1.2,
+            scale: { type: "spring", visualDuration: 0.4, bounce: 0.6 },
           }}>
           <Carousel
             opts={{
-              loop: true
+              loop: true,
             }}
             plugins={[
               AutoPlay({
                 delay: 5000,
-              })
-          ]}>
+              }),
+            ]}>
             <CarouselContent>
-              <CarouselItem>
-                <AspectRatio ratio={16/9}>
-                  <img src="/photos/home/carousel/slide-1.jpeg" alt="A Gobo on a curtain"/>
-                </AspectRatio>
-              </CarouselItem>
-              <CarouselItem>
-                <AspectRatio ratio={16/9}>
-                  <img src="/photos/home/carousel/slide-2.jpeg" alt="A Gobo on a curtain"/>
-                </AspectRatio>
-              </CarouselItem>
-              <CarouselItem>
-                <AspectRatio ratio={16/9}>
-                  <img src="/photos/home/carousel/slide-3.JPG" alt="A Gobo on a curtain"/>
-                </AspectRatio>
-              </CarouselItem>
-              <CarouselItem>
-                <AspectRatio ratio={16/9}>
-                  <img src="/photos/home/carousel/slide-4.jpeg" alt="A Gobo on a curtain"/>
-                </AspectRatio>
-              </CarouselItem>
-              <CarouselItem>
-                <AspectRatio ratio={16/9}>
-                  <img src="/photos/home/carousel/slide-5.jpeg" alt="A Gobo on a curtain"/>
-                </AspectRatio>
-              </CarouselItem>
-              <CarouselItem>
-                <AspectRatio ratio={16/9}>
-                  <img src="/photos/home/carousel/slide-6.jpeg" alt="A Gobo on a curtain"/>
-                </AspectRatio>
-              </CarouselItem>
-              <CarouselItem>
-                <AspectRatio ratio={16/9}>
-                  <img src="/photos/home/carousel/slide-7.jpeg" alt="A Gobo on a curtain"/>
-                </AspectRatio>
-              </CarouselItem>
+              {Array.from({ length: 7 }, (_, index) => (
+                <CarouselItem key={index}>
+                  <AspectRatio ratio={16 / 9}>
+                    <img
+                      src={`/photos/home/carousel/slide-${index + 1}.jpeg`}
+                      alt={`A Gobo on a curtain ${index + 1}`}
+                      className="object-cover"
+                    />
+                  </AspectRatio>
+                </CarouselItem>
+              ))}
             </CarouselContent>
-            <CarouselPrevious/>
+            <CarouselPrevious />
             <CarouselNext />
           </Carousel>
         </motion.div>
       </div>
       <div className="max-w-[864px] mx-auto pb-32">
         <motion.div
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-                duration: 1.2,
-                scale: { type: "spring", visualDuration: 0.4, bounce: 0.6 },
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 1.2,
+            scale: { type: "spring", visualDuration: 0.4, bounce: 0.6 },
           }}>
-        <section className="">
-          <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+          <section>
+            <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
               <div className="mr-auto place-self-center lg:col-span-7">
-                  <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white">About</h1>
-                  <p className="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl text-gray-400">Charlie Ward is a theatre technician based in the Leicestershire area. With experience in both theatre and other types of stage entertainment he is the go to place for all your technical needs. With having training on both EOS and MagicQ lighting boards, Charlie is well equipped to handle most lighting solutions.</p>
+                <h1 className="max-w-2xl mb-4 text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-extrabold tracking-tight leading-none text-white">
+                  About
+                </h1>
+                <p className="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl text-gray-400">
+                  Charlie Ward is a theatre technician based in the Leicestershire area. With experience in both theatre and other types of stage entertainment he is the go-to place for all your technical needs. With training on both EOS and MagicQ lighting boards, Charlie is well equipped to handle most lighting solutions.
+                </p>
               </div>
               <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-                <AspectRatio ratio={16/9}>
-                  <img src="/photos/home/mamma_mia.jpeg" alt="mockup" className="rounded-3xl" />
+                <AspectRatio ratio={16 / 9}>
+                  <img
+                    src="/photos/home/mamma_mia.jpeg"
+                    alt="mockup"
+                    className="rounded-3xl"
+                  />
                 </AspectRatio>
-              </div>                
-          </div>
-      </section>
+              </div>
+            </div>
+          </section>
         </motion.div>
       </div>
     </div>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,26 +1,28 @@
-'use client'
-import React from 'react'
-// import { Input } from './ui/input'
-import { Button } from './ui/button'
-import Image from 'next/image'
-// import { Moon, Sun, User} from "lucide-react"
-import Link from 'next/link'
-// import {
-//     DropdownMenu,
-//     DropdownMenuContent,
-//     DropdownMenuItem,
-//     DropdownMenuTrigger
-// } from "@/components/ui/dropdown-menu"
-// import { buttonVariants } from './ui/button'
+//navbar.tsx
+//Copyright (C) 2025  Charlie Ward GPL v3
+//Full License @ https://github.com/Charlie-Ward/cw-lighting/blob/main/LICENSE
+
+'use client';
+import React, { useState } from 'react';
+import { Button } from './ui/button';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Menu, SquareX } from 'lucide-react';
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <div className='max-w-[1280px] mx-auto'>
-            <div className='flex items-center py-4 gap-10 justify-between'>
+            <div className='flex items-center justify-between py-4'>
                 <Link href='/'>
                     <Image src='/photos/cwLighting-light.png' width={100} height={40} alt='Logo Image' />
                 </Link>
-                <div className='space-x-3'>
+                <div className='hidden md:flex space-x-3'>
                     <Button asChild>
                         <Link href='/'>Home</Link>
                     </Button>
@@ -31,9 +33,29 @@ const Navbar = () => {
                         <Link href='/contact'>Contact</Link>
                     </Button>
                 </div>
+                <Button onClick={toggleMenu} className='md:hidden p-2'>
+                    {isOpen ? (
+                        <SquareX />
+                    ) : (
+                        <Menu />
+                    )}
+                </Button>
             </div>
+            {isOpen && (
+                <div className='md:hidden flex flex-col space-y-2 py-2'>
+                    <Button asChild>
+                        <Link href='/'>Home</Link>
+                    </Button>
+                    <Button asChild>
+                        <Link href='/portfolio'>Portfolio</Link>
+                    </Button>
+                    <Button asChild>
+                        <Link href='/contact'>Contact</Link>
+                    </Button>
+                </div>
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
