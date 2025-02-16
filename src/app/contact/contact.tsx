b8dee4d4580bd7edc88255c19c80c3e5aabc27b8
Charlie Ward <charlieward0101@gmail.com>
2025-02-16 12:46:54 +0000

    Added direct contact info to contact format

--- src/app/contact/contact.tsx
+++ src/app/contact/contact.tsx
@@ -42,7 +42,10 @@ const Contact: FC = () => {
           </div>
           <button className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500'>Send Message</button>
         </form>
-      </div> 
+        <div className='text-white mt-4'>
+          Or you can send an email to charlie@cwlx.co.uk 
+        </div> 
+      </div>
     </div>
   );
 };
